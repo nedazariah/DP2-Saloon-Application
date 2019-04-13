13d7fda3c11bac3a73860f6980e6e2889c374670
NixSinyem <36840973+NixSinyem@users.noreply.github.com>
2019-04-13 21:46:05 +0800

    Update session_check.php

--- session_check.php
+++ session_check.php
@@ -4,18 +4,9 @@ session_start();
 
 //Check if the user has logged into the system, if not redirect to login page
 //Does not allow access to the system for outsiders
-if (!isset($_SESSION['userID'])) 
+if (!isset($_SESSION['logged']) && ($_SESSION['logged'] != true)) 
 {
 	header('location: login.php');
-	exit();
-}
-	
-//If logout is set (via clicking the Logout button), unsets the users logged in status, destroys the session and redirects to login page
-if(!empty($_GET['logout']) && $_GET['logout']=='true') 
-{ 
-	unset($_SESSION['userID']);
-	session_destroy();
-	header("location: login.php");
 	exit();
 }
 ?>
