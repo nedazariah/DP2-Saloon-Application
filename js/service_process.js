a6380cb0eee084128ae0e9f8ad81411c901c12ff
NixSinyem <36840973+NixSinyem@users.noreply.github.com>
2019-05-03 13:25:06 +0800

    Add files via upload

---
+++ js/service_process.js
@@ -0,0 +1,28 @@
+document.getElementById("formSubmit_back").onclick = function () {
+    location.href = "service_module_display.php";
+};
+
+document.getElementById("nform").onsubmit = function(){
+    
+    var message = "";
+    var serviceName = document.getElementById("serviceName").value;
+    var serviceCharge = document.getElementById("serviceCharge").value;
+    
+    if(serviceName == "" || serviceName == null)
+    {
+        alert("Service name cannot be empty");
+        return false;
+    }
+    else
+    {
+
+        if(serviceCharge == "" || serviceCharge == null || serviceCharge == 0)
+        {
+			return confirm("Are you sure with the fields?\nService charge is set at 0 or not filled.");
+        }
+        else
+        {
+            return confirm("Confirm form submission?");
+        }
+    }
+}
