e0635e8cf369799f1390fc697fefd48b73dcb492
NixSinyem <36840973+NixSinyem@users.noreply.github.com>
2019-05-06 13:27:42 +0800

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,5 +1,4 @@
 <?php
-
 $hasError = false;
 
 function SanitizeData($data){
@@ -9,7 +8,7 @@ function SanitizeData($data){
   return $data;
 }
 
-/* Turn off error reporting */
+/* Turn off error reporting*/ 
 error_reporting(0);
 
 /* Declaring variables for database connection */
@@ -20,6 +19,11 @@ $dbname = "salon";
 
 /* Create connection with database */
 $connect = @mysqli_connect($servername, $username, $password, $dbname);
+
+/* Function that escapes string for querying */
+function MysqliEscape($connect, $var){
+    return mysqli_real_escape_string($connect, $var);
+}
 
 if(!$connect){
 	die("Connection failed: " . mysqli_connect_error());
