2e0fa068359be12ffcdf8ce757ac3677748f31ad
nedazariah <43191051+nedazariah@users.noreply.github.com>
2019-05-06 20:56:24 +0800

    Check Primary Key
    If staffID is in user table, will delete in user table then delete in staff table.

--- deletestaff.php
+++ deletestaff.php
@@ -3,12 +3,27 @@
     include "session_check.php";
     $staffID = $_GET['staffID'];
 
-    $sql = "DELETE FROM staff WHERE staffID ='". $staffID ."'";
+    $check = "SELECT * FROM user WHERE userID ='". $staffID . "'";
 
-    if (mysqli_query($connect,$sql)){
-        echo "Success";
-        header("location: displaystaff.php");
-        ob_enf_fluch();
+    if($result = mysqli_query($connect, $check)) {
+            if(mysqli_num_rows($result) > 0) {
+                $sql1 = "DELETE FROM user WHERE userID ='". $staffID ."'";
+                if(mysqli_query($connect,$sql1)) {
+                    $sql2 = "DELETE FROM staff WHERE staffID ='". $staffID ."'";
+                    if (mysqli_query($connect,$sql2)){
+                        echo "Success";
+                        header("location: displaystaff.php");
+                        ob_enf_fluch();
+                    }
+                }
+            } else {
+                $sql2 = "DELETE FROM staff WHERE staffID ='". $staffID ."'";
+                if (mysqli_query($connect,$sql2)){
+                    echo "Success";
+                    header("location: displaystaff.php");
+                    ob_enf_fluch();
+                }
+            }        
     }
 
 ?>
