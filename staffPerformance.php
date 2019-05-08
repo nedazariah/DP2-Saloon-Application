784590dc734207af8eaece46d1534067eef6b85e
100082671 <43191576+100082671@users.noreply.github.com>
2019-05-08 14:23:43 +0800

    Add files via upload

--- staffPerformance.php
+++ staffPerformance.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en" data-ng-app="">
+<html lang="en" data-ng-app="myApp">
 <?php
     include "session_check.php";
 
@@ -41,7 +41,7 @@
 	</style>
 </head>
 
-<body >
+<body data-ng-controller="myCtrl">
 	<div class="row">
 		<div class="col-md-2">
 			<div class="sideNav">
@@ -85,8 +85,10 @@
     if (!$connect){
         die("Connection failed: " . mysqli_connect_error());
     }
+	$selectstaff = "SELECT * from staff";
     $select = "SELECT * from staff_performance";
     $result = mysqli_query($connect,$select);
+	$resultstaff = mysqli_query($connect,$selectstaff);
     $array = array();
     class staffPerformance{
         public $performanceID;
@@ -95,7 +97,19 @@
         public $daysWorked;
         public $custServed;
     }
+	class staff{
+        public $staffID;
+        public $staffName;
+    }
+	while ($rowstaff = mysqli_fetch_assoc($resultstaff)){
+            $staff = new staff();
 
+ 
+        $staff->staffID = $rowstaff["staffID"];
+        $staff->staffName = $rowstaff["staffName"];
+     
+           $arraystaff[] = $staff;
+}	
     while ($row = mysqli_fetch_assoc($result)){
             $staffPerf = new staffPerformance();
 
@@ -109,9 +123,9 @@
 
     ?>
 			<h1>Staff Performance Report</h1>
-			<div class="col-md-12" data-ng-init="staffPerfs=<?php echo htmlspecialchars(json_encode($array));?>">
+			<div class="col-md-12" data-ng-init="staffPerfsInit(<?php echo htmlspecialchars(json_encode($array));?>,<?php echo htmlspecialchars(json_encode($arraystaff));?>)">
 				<div class="row">
-					<div class="col-md-6">
+					<div class="col-md-12">
 						<h2 class="text-center">Staff Performance</h2>
 						<div class="table-responsive">
 							<table class="table table-striped table-bordered">
@@ -132,6 +146,10 @@
 							</table>
 						</div>
 					</div>
+					<div class="col-md-12">
+						<h2 class="text-center">Chart</h2>
+						<div id="staffChart"></div>
+					</div>
 				</div>
 			</div>
 
@@ -140,7 +158,101 @@
 	</div>
 	<script src="js/jquery.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
+	<script src="framework/code/highcharts.js"></script>
 	<script src="js/angular.min.js"></script>
+
+	<script>
+		var app = angular.module("myApp", []);
+		app.controller("myCtrl", function($scope) {
+			$scope.staffPerfsInit = function(input, input1) {
+				$scope.staffdayswork = [];
+				$scope.staffarray = input1;
+				$scope.daysperformance = [];
+
+				$scope.staffPerfs = input;
+
+
+				$scope.daysworkArray = new Array($scope.staffarray.length);
+				$scope.custServeArray = new Array($scope.staffarray.length);
+				for ($scope.i = 0; $scope.i < $scope.staffarray.length; $scope.i = $scope.i + 1) {
+					$scope.daysworkArray[$scope.i] = new Array();
+					$scope.custServeArray[$scope.i] = new Array();
+				}
+				for ($scope.i = 0; $scope.i < $scope.staffPerfs.length; $scope.i++) {
+					for ($scope.j = 0; $scope.j < $scope.staffarray.length; $scope.j++) {
+						if ($scope.staffPerfs[$scope.i].staffID === $scope.staffarray[$scope.j].staffID) {
+							$scope.staffdayswork.push(parseInt($scope.staffPerfs[$scope.i].daysWorked));
+							$scope.daysworkArray[$scope.j].push(parseInt($scope.staffPerfs[$scope.i].daysWorked));
+							$scope.custServeArray[$scope.j].push(parseInt($scope.staffPerfs[$scope.i].custServed))
+						}
+					}
+				}
+				for ($scope.f = 0; $scope.f < $scope.staffarray.length; $scope.f++) {
+					$scope.daysperformance.push({
+						name: $scope.staffarray[$scope.f].staffID,
+						data: $scope.daysworkArray[$scope.f],
+						stack: 'Days Worked'
+					}, {
+						name: $scope.staffarray[$scope.f].staffID,
+						data: $scope.custServeArray[$scope.f],
+						stack: 'Customers Served'
+					});
+				}
+
+				Highcharts.chart('staffChart', {
+
+					chart: {
+						type: 'column'
+					},
+
+					title: {
+						text: 'Staff Performance'
+					},
+
+					xAxis: {
+						categories: ['Jan',
+							'Feb',
+							'Mar',
+							'Apr',
+							'May',
+							'Jun',
+							'Jul',
+							'Aug',
+							'Sep',
+							'Oct',
+							'Nov',
+							'Dec'
+						]
+					},
+
+					yAxis: {
+						allowDecimals: false,
+						min: 0,
+						title: {
+							text: 'Figures'
+						}
+					},
+
+					tooltip: {
+						formatter: function() {
+							return '<b>' + this.series.options.stack + '</b><br/>' +
+								this.series.name + ': ' + this.y + '<br/>' +
+								'Total: ' + this.point.stackTotal;
+						}
+					},
+
+					plotOptions: {
+						column: {
+							stacking: 'normal'
+						}
+					},
+
+					series: $scope.daysperformance
+				});
+			}
+		});
+
+	</script>
 </body>
 
 </html>
