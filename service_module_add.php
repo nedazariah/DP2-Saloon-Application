02b41fbfc5c62fd010eedbfa474f6f513fde6d52
NixSinyem <36840973+NixSinyem@users.noreply.github.com>
2019-05-10 16:29:36 +0800

    Updated form layout and style

--- service_module_add.php
+++ service_module_add.php
@@ -13,8 +13,16 @@ include "session_check.php";
     <link href="css/bootstrap.min.css" rel="stylesheet" />
     
 	<!--Custom Style-->
-	<link href="css/nstyle.css" rel="stylesheet"/>
-	<link href="css/nav_style.css" rel="stylesheet"/> 
+	<link href="css/nav_style.css" rel="stylesheet"/>
+    <style>
+        h1{
+            text-align: center;
+        }
+        
+        .btn{
+            width: 10em;
+        }
+    </style>  
     
     <!-- jQuery – required for Bootstrap's JavaScript plugins) -->
     <script src="js/jquery.min.js"></script>
@@ -24,45 +32,68 @@ include "session_check.php";
 </head>
 <body> 
     
-    <div id="npage">
+    <div class="container">
         <div class="row">
                         <div class="col-md-2">
                 <div class="sideNav">
-                    <?php
-                        include "navigation.php";
-                    ?>
+                    <ul class="nav nav-pills nav-stacked">
+                        <li class="dropdown-btn"><a href="#">Appointment</a>
+                            <ul class="nav nav-pills nav-stacked dropdown-container">
+                                <li><a href="appointmentform.php">Add Appointment</a></li>
+                                <li><a href="pendingappointment.php">Pending Appointments</a></li>
+                                <li><a href="appointment.php">All Appointments</a></li>
+                            </ul>
+                        </li>
+                        
+                        <li><a href="displayCustomer.php">Customers</a></li>
+                        <li><a href="stock_module_display.php">Stock</a></li> 
+                        <?php
+				            if($role == "Manager"){ 
+                                echo "<li><a href='service_module_display.php'>Services</a></li>";
+                                echo "<li><a href='displaystaff.php'>Staff</a></li>";
+                                echo "<li class='dropdown-btn'><a href='#'>Reports</a>";
+                                echo   "<ul class='nav nav-pills nav-stacked dropdown-container'>";
+                                echo       "<li><a href='item_sales_report.php'>Items Sales</a></li>";
+                                echo       "<li><a href='#'>Staff Performance</a></li>";
+                                echo   "</ul>";
+                                echo "</li>";
+				            }
+						    echo ("<script>console.log('Role: ".$role."')</script>");
+				        ?>
+                    </ul>
+                    
+                    <div class="btm-menu">
+                        <ul class="nav nav-pills nav-stacked">
+                            <li><a href="user_module_account_setting.php">Account</a></li>
+                            <li><a href="logout.php">Logout</a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>           
            
             <div class="col-md-10">
                
+                <h1>New Service</h1>
+                
                 <form id="nform" name="nform" method="post" action="service_module_add.php">
                    
-                    <div class="row">
-                        <div class="col-md-12">
-                            <h1>New Service</h1>                    
-                        </div>
+                    <div class="form-group">
+                        <label for="serviceName">Service Name:</label>
+                        <input type="text" id="serviceName" name="serviceName" class="form-control" required="required" maxlength="30"/>
+                    </div>
+                                                
+                    <div class="form-group">
+                        <label for="serviceCharge">Service Fee:</label>
+                        <input type="number" id="serviceCharge" name="serviceCharge" class="form-control" maxlength="6"/>
+                    </div>    
+                    
+                    <div class="form-group text-center">        
+                        <button type="submit" name="add" class="btn btn-primary">Add</button> 
+                        <a href="service_module_display.php" class="btn btn-default">Go Back</a>
                     </div>
 
-                    <div class="row">
-                        <div class="col-md-12">
-                            <input type="text" id="serviceName" name="serviceName" placeholder="Service Name (required)" required="required" maxlength="30"/>
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-md-12">
-                            <label for="serviceCharge">Service Fee:</label><input type="number" id="serviceCharge" name="serviceCharge" maxlength="6"/>
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-md-12">
-                            <button type="submit" name="add" id="formSubmit_change">Add</button> 
-                            <a href="service_module_display.php" id="formSubmit_back">Go Back</a>
-                        </div>
-                    </div>
-                </form>                       
+                </form>   
+                              
             </div>
         </div>
 <?php
