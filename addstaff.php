7be8329e229294a856b9e26ee666b49dffb6f79e
nedazariah <43191051+nedazariah@users.noreply.github.com>
2019-05-11 10:40:00 +0800

    Updated Layout
    Changed the layout of the form to be uniform with others.

--- addstaff.php
+++ addstaff.php
@@ -6,6 +6,7 @@
 <?php
 include "session_check.php";
 ?>
+
 <head>
     <meta charset="UTF-8">
     <meta name="language" content="english" />
@@ -13,7 +14,6 @@ include "session_check.php";
     <meta name="description" content="Style and Smile Saloon House Adding Staff Form" />
     <title>Add Staff</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="css/nav_style.css">
 </head>
 
@@ -30,80 +30,77 @@ include "session_check.php";
 
             <div class="col-md-10">
                 <h1>Add Staff</h1>
-                <br>
-                <div class="formContainer">
-                    <form method="post" action="addstaff.php" onsubmit="return validate('add')">
-                        <div class="row">
-                            <div class="col-md-12">
-                                <input type="text" name="staffName" id="staffName" class="fWidth" placeholder="Full Name">
-                                <span id="staffNameError" class="error"></span>
-                            </div>
+                <div>
+                    <form method="post" action="addstaff.php" onsubmit="return validate('add')" name="asForm" novalidate="novalidate">
+
+                        <div class="form-group">
+                            <label for="staffName">Staff Name</label>
+                            <input type="text" name="staffName" id="staffName" class="form-control" placeholder="Full Name">
+                            <span id="staffNameError" class="text-danger"></span>
                         </div>
-                        <br>
-                        <div class="row">
-                            <div class="col-md-12">
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <label for="staffDOB">Date of Birth: </label>
-                                    </div>
-                                    <div class="col-md-6">
-                                        Gender:
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <input type="date" name="staffDOB" class="fWidth" id="staffDOB">
-                                        <span id="staffDOBError" class="error"></span>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <input type="radio" name="staffGender" id="genderMale" value="M"> <label for="genderMale">Male</label>
-                                        <input type="radio" name="staffGender" id="genderFemale" value="F"> <label for="genderFemale">Female</label>
-                                        <span id="staffGenderError" class="error"></span>
-                                    </div>
-                                </div>
-                            </div>
+
+
+                        <div class="form-group">
+                            <label for="staffDOB">Date of Birth</label>
+                            <input type="date" name="staffDOB" id="staffDOB" class="form-control">
+                            <span id="staffDOBError" class="text-danger"></span>
                         </div>
-                        <br>
-                        <div class="row">
-                            <div class="col-md-6">
-                                <input type="text" name="staffPhone" id="staffPhone" class="fWidth" placeholder="Phone Number">
-                                <span id="staffPhoneError" class="error"></span>
-                            </div>
-                            <div class="col-md-6">
-                                <input type="email" name="staffEmail" id="staffEmail" class="fWidth" placeholder="Email">
-                                <span id="staffEmailError" class="error"></span>
-                            </div>
+
+
+                        <div class="form-group">
+                            <label>Gender</label>
+                            <br>
+                            <label class="radio-inline">
+                                <input type="radio" name="staffGender" id="genderMale" value="M">Male
+                            </label>
+                            <label class="radio-inline">
+                                <input type="radio" name="staffGender" id="genderFemale" value="F">Female
+                            </label>                            
+                            <br>
+                            <span id="staffGenderError" class="text-danger"></span>
                         </div>
-                        <br>
-                        <div class="row">
-                            <div class="col-md-12">
-                                <label for="staffRole">Role:</label>
-                                <select name="staffRole" id="staffRole" class="fWidth">
-                                    <option value="">Select a Role</option>
-                                    <option value="Manager">Manager</option>
-                                    <option value="Receptionist">Receptionist</option>
-                                    <option value="Hairdresser">Hairdresser</option>
-                                </select>
-                                <span id="staffRoleError" class="error"></span>
-                            </div>
+
+
+                        <div class="form-group">
+                           <label for="staffPhone">Staff Phone No.</label>
+                            <input type="text" name="staffPhone" id="staffPhone" class="form-control" placeholder="Phone Number">
+                            <span id="staffPhoneError" class="text-danger"></span>
                         </div>
-                        <br>
-                        <div class="row">
-                            <div class="col-md-12">
-                                <label for="staffAdd">Address: </label>
-                                <br>
-                                <textarea rows="6" name="staffAdd" id="staffAdd" class="fWidth" placeholder="Address"></textarea>
-                            </div>
+                        
+                        <div class="form-group">
+                           <label for="staffEmail">Staff Email</label>
+                            <input type="email" name="staffEmail" id="staffEmail" class="form-control" placeholder="Email">
+                            <span id="staffEmailError" class="text-danger"></span>
                         </div>
-                        <br>
-                        <div class="row">
-                            <div class="col-md-12">
-                                <input type="submit" value="Submit" class="submit" name="submit">
-                                <button type="button" value="Cancel" class="cancel" onclick="window.location.replace('displaystaff.php')">Cancel</button>
-                            </div>
+
+
+                        <div class="form-group">
+                            <label for="staffRole">Role</label>
+                            <select name="staffRole" id="staffRole" class="form-control">
+                                <option value="">Select a Role</option>
+                                <option value="Manager">Manager</option>
+                                <option value="Receptionist">Receptionist</option>
+                                <option value="Hairdresser">Hairdresser</option>
+                            </select>
+                            <span id="staffRoleError" class="text-danger"></span>
                         </div>
+                        
+                        <div class="form-group">
+                            <label for="staffAdd">Address</label>
+                            <textarea rows="3" name="staffAdd" id="staffAdd" class="form-control" placeholder="Address"></textarea>
+                        </div>
+
+                        <div class="form-group text-center">
+                            <input type="submit" value="Submit" class="btn btn-primary" name="submit">
+                            <button type="button" value="Cancel" class="btn btn-default" onclick="window.location.replace('displaystaff.php')">Cancel</button>
+                        </div>
+
                     </form>
                 </div>
+
+
+
+
             </div>
         </div>
     </div>
@@ -111,7 +108,7 @@ include "session_check.php";
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="staffFormValidation.js"></script>
-   
+
 </body>
 <?php
     if(isset($_POST['submit'])) {
