f113bff273c452d6499fee4cadbad35f9e251216
NixSinyem <36840973+NixSinyem@users.noreply.github.com>
2019-05-11 14:58:44 +0800

    Refined error output

--- login.php
+++ login.php
@@ -19,6 +19,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     
+    <!--Custom style-->
     <link rel="stylesheet" href="css/login_style.css?v=<?php echo time(); ?>"/>
 
     <!-- Bootstrap -->
@@ -50,11 +51,15 @@
                     <div class="form-group">
                         <label for="user" class="col-md-1 col-md-offset-4">User ID: </label>
                         <input type="text" name="user" id="user" maxlength="11" class="col-md-3"/>
+                        <br/> 
+                        <span id="userIDError" class="text-danger text-left col-md-3 col-md-offset-5"></span>
                     </div>
                        
                     <div class="form-group">   
                         <label for="pws" class="col-md-1 col-md-offset-4">Password: </label>
                         <input type="password" name="pws" id="pws" maxlength="20" class="col-md-3"/>
+                        <br/> 
+                        <span id="pwsError" class="text-danger text-left col-md-3 col-md-offset-5"></span>
                     </div>
                     
                     <div class="form-group text-center">
@@ -74,23 +79,29 @@
 <?php
 session_start(); 
 
+//Message variable to be used for back-end validation
+$user_error = $pws_error = $login_error = "";
+
+//Form data processing block
 if($_SERVER["REQUEST_METHOD"] == "POST")
 { 
-	//Validate email	
-    $input_user = SanitizeData($_POST["user"]);
-	if(empty($input_user)){
-		echo "Error: Username not found." . "</br>";
-		$hasError = true;
-    } 
-	else
-	{
+	//Validate user id
+    $input_user = SanitizeData($_POST["user"]);    
+    if(empty($input_user))
+    {
+        $user_error = "User ID not found.";
+        $hasError = true;
+    }
+    else 
+    {
         $user = $input_user;
-    }	
-	
+    } 
+    
 	//Validate password
     $input_pws = SanitizeData($_POST["pws"]);
-    if(empty($input_pws)){
-		echo "Error: Password not found." . "</br>";
+    if(empty($input_pws))
+    {
+		$pws_error = "Password not found.";
 		$hasError = true;
     }
 	else
@@ -99,23 +110,31 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
     }		
 
 	if(!($hasError))
-	{
+	{ 
 		$sql = "SELECT * FROM user WHERE userID ='$user'";
-		$results = mysqli_query($connect, $sql);
-		$row = mysqli_fetch_array($results);
-
-		if(($row['userID'] == $user) && password_verify($pws, $row['userPass']))
-		{
-			$_SESSION['logged'] = true;	
-            $_SESSION['loggedUser'] = $user; //Added by Almira
-			mysqli_free_result($results);
-			header("location: pendingappointment.php"); 
-			exit();
-		}
-		else
-		{ 
-			echo "Login Failed.";
-		}
+        
+        if($result = mysqli_query($connect, $sql))
+        {
+            $row = mysqli_fetch_array($result); 
+                        
+            if(password_verify($pws, $row['userPass']))
+            {
+                $_SESSION['logged'] = true;	
+                $_SESSION['loggedUser'] = $user; //Added by Almira
+                mysqli_free_result($results);
+                header("location: pendingappointment.php"); 
+                exit();
+            }
+            else
+            { 
+                mysqli_free_result($results);
+                $login_error = "Login failed.";
+            }            
+        }
+        else
+        {
+            $login_error = "Login failed.";
+        }
 	}
 }
 ?>                
@@ -125,5 +144,26 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
         
     </div>
     
+<!--Back-end form validation error output-->
+<script>
+    var user_error = "<?php echo $user_error; ?>";
+    var pws_error = "<?php echo $pws_error; ?>";
+    var login_error = "<?php echo $login_error; ?>";
+    
+    if(user_error.length !== 0)
+    {
+        document.getElementById("userIDError").innerHTML = user_error;
+    }
+    
+    if(pws_error.length !== 0)
+    {
+        document.getElementById("pwsError").innerHTML = pws_error;
+    }    
+    
+    if(login_error.length !== 0)
+    {
+        alert(login_error);
+    }
+</script>   
 </body>
 </html>
