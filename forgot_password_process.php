cc4d5bb39712cb19a20ce2ab9116c44f69c99a7d
NixSinyem <36840973+NixSinyem@users.noreply.github.com>
2019-05-11 15:01:10 +0800

    Refined error output

--- forgot_password_process.php
+++ forgot_password_process.php
@@ -2,7 +2,7 @@
     require_once "dbconnect.php";
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html data-ng-app="" lang="en">
 <head>
     <title>Forgot Password</title>
     
@@ -31,12 +31,15 @@
 
 <body>
 <?php
+//Message variable to be used for back-end validation
+$pws_error = $repws_error = $sa_error = $server_comm_error = $verify_error = "";
+    
 //Form data processing block
 if($_SERVER["REQUEST_METHOD"] == "POST") 
 {
     //Initialize all inventory fields
     $userID = SanitizeData($_POST["target"]);
-    $userNPass = $message = "";   
+    $userNPass = "";   
     
     $hasError = false;
     
@@ -44,14 +47,15 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
     $input_userSA = SanitizeData($_POST["sa"]);
     $storedSA = SanitizeData($_POST["ssa"]);
     
-    if(empty($input_userSA)){
+    if(empty($input_userSA))
+    {
 		$hasError = true;
-        $message .= "Error: Input answer for security question not found </br>";
+        $sa_error = "Security question not found.";
     }
 	else if($input_userSA != $storedSA)
     {
         $hasError = true;
-        $message .= "Error: Security answer is incorrect </br>";   
+        $verify_error = "Verification failed.";   
 	}	
     //Processing User SA <End>
         
@@ -59,26 +63,40 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
     $input_userNPass = SanitizeData($_POST["pws"]);
     $input_userCNPass = SanitizeData($_POST["re_pws"]);
     
-    if(empty($input_userNPass)){
-		$hasError = true;
-        $message .= "Error: New password not found </br>";
-    }
-    if(empty($input_userCNPass)){
-		$hasError = true;
-        $message .= "Error: Re-entered password not found </br>";
-    }
-	else if($input_userNPass != $input_userCNPass)
+    if(empty($input_userNPass))
     {
-        $hasError = true;
-        $message .= "Error: Entered passwords do not match </br>";        
+		$hasError = true;
+        $pws_error = "New password not found.";
     }
-    else
+    if(empty($input_userCNPass))
+    {
+		$hasError = true;
+        $repws_error = "Re-entered password not found.";
+    }
+	else
 	{
         $userNPass = MysqliEscape($connect, $input_userNPass);
 	}	
     //Processing password <End>
     
-    submit($userID, $userNPass, $hasError, $connect, $message);
+    //If there is no error, proceed to insert data
+	if(!($hasError))
+	{
+        $pws_hash = password_hash($userNPass, PASSWORD_DEFAULT);
+        
+		$sql = "UPDATE user SET userPass = '$pws_hash' WHERE userID = " . $userID;
+		
+		if (mysqli_query($connect, $sql)) {
+			header("location: login.php");
+			exit();
+		}
+		else 
+		{
+			$server_comm_error = "Error: " . $sql . "</br>" . die(mysqli_error($connect));
+		} 		
+	}
+    
+    mysqli_close($connect);	   
 }
 else
 {
@@ -99,7 +117,7 @@ else
         }
         else 
         {
-            alertUser("Error: " . $sql . "</br>" . die(mysqli_error($connect)));
+            $server_comm_error = "Error: " . $sql . "</br>" . die(mysqli_error($connect));//
         }
 
         mysqli_close($connect);	
@@ -128,33 +146,57 @@ else
                     <!--User ID-->
                     <div class="form-group">
                         <label for="user">User ID: </label>
-                        <input type="text" name="user" id="user" maxlength="11" class="form-control" disabled="disabled" value="<?php echo $target ?>"/>
-                        <input type="hidden" name="target" value="<?php echo $target ?>"/>
+                        <input type="text" name="user" id="user" maxlength="11" class="form-control" disabled="disabled" 
+                        value="<?php 
+                                if($_SERVER["REQUEST_METHOD"] == "POST")
+                                    echo $_POST["target"];
+                                else 
+                                    echo $target; ?>"/>
+                        <input type="hidden" name="target" 
+                        value="<?php 
+                               if($_SERVER["REQUEST_METHOD"] == "POST")
+                                    echo $_POST["target"];
+                                else 
+                                    echo $target; ?>"/>
                     </div>
                     
                     <!--Security Question-->
                     <div class="form-group">
                         <label for="sq">Security Question: </label>
-                        <textarea name="sq" id="sq" maxlength="250" class="form-control" disabled="disabled"><?php echo $SQ ?></textarea>
-                        <input type="hidden" name="ssa" value="<?php echo $SA ?>"/>
+                        <textarea name="sq" id="sq" maxlength="250" class="form-control" disabled="disabled"><?php 
+                                if($_SERVER["REQUEST_METHOD"] == "POST")
+                                    echo $_POST["ssa"];
+                                else 
+                                    echo $SA; ?></textarea>
+                        <input type="hidden" name="ssa" 
+                        value="<?php 
+                                if($_SERVER["REQUEST_METHOD"] == "POST")
+                                    echo $_POST["ssa"];
+                                else 
+                                    echo $SA; ?>"/>
                     </div>
                     
                     <!--Security Answer-->                                  
                     <div class="form-group">
                         <label for="sa">Answer: </label>
-                        <textarea name="sa" id="sa" class="form-control" maxlength="250"></textarea>
+                        <textarea name="sa" id="sa" class="form-control" maxlength="250"><?php 
+                                if($_SERVER["REQUEST_METHOD"] == "POST")
+                                    echo $_POST["sa"];?></textarea>
+                        <span class="text-danger" id="ans_error"></span>
                     </div>
                     
                     <!--New Password-->
                     <div class="form-group">
                         <label for="pws">New Password: </label>
-                        <input type="password" name="pws" class="form-control" id="pws" maxlength="20"/>
+                        <input type="password" name="pws" class="form-control" id="pws" value="<?php echo $_POST["pws"]; ?>" maxlength="20"/>
+                        <span class="text-danger" id="pws_error"></span>
                     </div>
 
                     <!--Confirm password-->
                     <div class="form-group">
                         <label for="re_pws">Re-Enter Password: </label>
-                        <input type="password" name="re_pws" class="form-control" id="re_pws" maxlength="20"/>
+                        <input type="password" name="re_pws" class="form-control" id="re_pws" value="<?php echo $_POST["re_pws"]; ?>" maxlength="20"/>
+                        <span class="text-danger" id="repws_error"></span>
                     </div>                       
                 
                     <!--Buttons-->
@@ -169,48 +211,37 @@ else
             
         </div>
         
-        <div class="row">
-            
-            <div class="col-md-12" id="echo">
-               
-<?php
-function submit($userID, $userNPass, $hasError, $connect, $message)
-{
-	//If there is no error, proceed to insert data
-	if(!($hasError))
-	{
-        $pws_hash = password_hash($userNPass, PASSWORD_DEFAULT);
-        
-		$sql = "UPDATE user SET userPass = '$pws_hash' WHERE userID = " . $userID;
-		
-		if (mysqli_query($connect, $sql)) {
-			header("location: login.php");
-			exit();
-		}
-		else 
-		{
-			alertUser("Error: " . $sql . "</br>" . die(mysqli_error($connect)));
-		} 		
-	}
-	else
-	{
-        $message .= "Error: Please check input fields.";
-        alertUser($message);
-	}	
-    
-    mysqli_close($connect);	    
-}
-
-function alertUser($str){
-    echo $str;
-}
-?>            
-                    
-            </div>
-            
-        </div>
-        
     </div>
+
+<!--Back-end form validation error output-->
+<script>
+    var pws_error = "<?php echo $pws_error; ?>";
+    var repws_error = "<?php echo $repws_error; ?>";
+    var sa_error = "<?php echo $sa_error; ?>";
+    var verify_error = "<?php echo $verify_error; ?>";
+    var server_error = "<?php echo $server_comm_error; ?>";
     
+    function checkError(str, id)
+    {
+        if(str.length !== 0)
+        {
+            document.getElementById(id).innerHTML = str;
+        }
+    }
+    
+    checkError(pws_error, "pws_error");
+    checkError(repws_error, "repws_error");
+    checkError(sa_error, "ans_error");
+    
+    if(server_error.length !== 0)
+    {
+        alert(server_error);
+    }
+    
+    if(verify_error.length !== 0)
+    {
+        alert(verify_error);        
+    }
+</script>    
 </body>
 </html>
