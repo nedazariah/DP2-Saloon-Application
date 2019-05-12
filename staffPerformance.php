9985adb85c6fb9907a5f04241506f91808ccae73
100082671 <43191576+100082671@users.noreply.github.com>
2019-05-12 21:28:06 +0800

    Add files via upload
    updated staff performance generating report

--- staffPerformance.php
+++ staffPerformance.php
@@ -10,7 +10,7 @@
 	<meta name="viewport" content="width=device-width, initialscale=1.0" />
 	<meta charset="utf-8" />
 	<link href="css/bootstrap.min.css" rel="stylesheet" />
-    <script src="https://code.highcharts.com/highcharts.src.js"></script>
+	<script src="https://code.highcharts.com/highcharts.src.js"></script>
 	<link href="css/nav_style.css" rel="stylesheet" />
 	<style>
 		table,
@@ -124,20 +124,20 @@
     ?>
 			<h1>Staff Performance Report</h1>
 			<div class="col-md-12" data-ng-init="staffPerfsInit(<?php echo htmlspecialchars(json_encode($array));?>,<?php echo htmlspecialchars(json_encode($arraystaff));?>)">
-			
-			<div class="row">
-			<div class="col-md-12 text-right">
-			    <a href='staffPerformanceEntry.php' class='btn btn-default'>Add New Entry</a>
-			</div>
-			</div>
-			
+
+				<div class="row">
+					<div class="col-md-12 text-right">
+						<a href='staffPerformanceEntry.php' class='btn btn-default'>Add New Entry</a>
+					</div>
+				</div>
+
 				<div class="row">
 					<div class="col-md-12">
 						<h2 class="text-center">Staff Performance</h2>
 						<div class="table-responsive">
 							<table class="table table-striped table-bordered">
 								<tr>
-									<th>Performance ID</th>
+									<th>Performance ID{{yearArray}}</th>
 									<th>Staff ID</th>
 									<th>Month Year</th>
 									<th>Days Worked</th>
@@ -153,7 +153,15 @@
 							</table>
 						</div>
 					</div>
+
 					<div class="col-md-12">
+						<select data-ng-model="yearSelect" data-ng-change="changeYear(<?php echo htmlspecialchars(json_encode($array));?>,<?php echo htmlspecialchars(json_encode($arraystaff));?>)">
+							<option value="">--Select Year For Report--</option>
+							<option data-ng-repeat="y in yearArray" value="{{y}}">{{y}}</option>
+						</select>
+
+					</div>
+					<div class="col-md-12" data-ng-show="yearSelect">
 						<h2 class="text-center">Chart</h2>
 						<div id="staffChart"></div>
 					</div>
@@ -171,13 +179,24 @@
 	<script>
 		var app = angular.module("myApp", []);
 		app.controller("myCtrl", function($scope) {
+			$scope.changeYear = function(input, input1, index) {
+				$scope.staffPerfsInit(input, input1);
+			}
 			$scope.staffPerfsInit = function(input, input1) {
 				$scope.staffdayswork = [];
 				$scope.staffarray = input1;
 				$scope.daysperformance = [];
-
+				$scope.isNewYear = false;
 				$scope.staffPerfs = input;
+				$scope.yearArray = [];
+				$scope.yearArray.push($scope.staffPerfs[0].monthYear.slice(0, 4));
+				for ($scope.i = 0; $scope.i < $scope.staffPerfs.length; $scope.i++) {
+					if (!$scope.yearArray.includes($scope.staffPerfs[$scope.i].monthYear.slice(0, 4))) {
+						$scope.yearArray.push($scope.staffPerfs[$scope.i].monthYear.slice(0, 4));
+					}
 
+				}
+				$scope.yearArray.sort();
 
 				$scope.daysworkArray = new Array($scope.staffarray.length);
 				$scope.custServeArray = new Array($scope.staffarray.length);
@@ -185,15 +204,61 @@
 					$scope.daysworkArray[$scope.i] = new Array();
 					$scope.custServeArray[$scope.i] = new Array();
 				}
+				for ($scope.i = 0; $scope.i < $scope.staffarray.length; $scope.i++) {
+					for ($scope.j = 0; $scope.j < 12; $scope.j++) {
+						$scope.daysworkArray[$scope.i].push(0);
+						$scope.custServeArray[$scope.i].push(0);
+					}
+				}
+
 				for ($scope.i = 0; $scope.i < $scope.staffPerfs.length; $scope.i++) {
 					for ($scope.j = 0; $scope.j < $scope.staffarray.length; $scope.j++) {
 						if ($scope.staffPerfs[$scope.i].staffID === $scope.staffarray[$scope.j].staffID) {
 							$scope.staffdayswork.push(parseInt($scope.staffPerfs[$scope.i].daysWorked));
-							$scope.daysworkArray[$scope.j].push(parseInt($scope.staffPerfs[$scope.i].daysWorked));
-							$scope.custServeArray[$scope.j].push(parseInt($scope.staffPerfs[$scope.i].custServed))
+							if ($scope.staffPerfs[$scope.i].monthYear.slice(0, 4) === $scope.yearSelect) {
+								if ($scope.staffPerfs[$scope.i].monthYear.slice(-2) === "01") {
+									$scope.daysworkArray[$scope.j][0] = parseInt($scope.staffPerfs[$scope.i].daysWorked);
+									$scope.custServeArray[$scope.j][0] = parseInt($scope.staffPerfs[$scope.i].custServed);
+								} else if ($scope.staffPerfs[$scope.i].monthYear.slice(-2) === "02") {
+									$scope.daysworkArray[$scope.j][1] = parseInt($scope.staffPerfs[$scope.i].daysWorked);
+									$scope.custServeArray[$scope.j][1] = parseInt($scope.staffPerfs[$scope.i].custServed);
+								} else if ($scope.staffPerfs[$scope.i].monthYear.slice(-2) === "03") {
+									$scope.daysworkArray[$scope.j][2] = parseInt($scope.staffPerfs[$scope.i].daysWorked);
+									$scope.custServeArray[$scope.j][2] = parseInt($scope.staffPerfs[$scope.i].custServed);
+								} else if ($scope.staffPerfs[$scope.i].monthYear.slice(-2) === "04") {
+									$scope.daysworkArray[$scope.j][3] = parseInt($scope.staffPerfs[$scope.i].daysWorked);
+									$scope.custServeArray[$scope.j][3] = parseInt($scope.staffPerfs[$scope.i].custServed);
+								} else if ($scope.staffPerfs[$scope.i].monthYear.slice(-2) === "05") {
+									$scope.daysworkArray[$scope.j][4] = parseInt($scope.staffPerfs[$scope.i].daysWorked);
+									$scope.custServeArray[$scope.j][4] = parseInt($scope.staffPerfs[$scope.i].custServed);
+								} else if ($scope.staffPerfs[$scope.i].monthYear.slice(-2) === "06") {
+									$scope.daysworkArray[$scope.j][5] = parseInt($scope.staffPerfs[$scope.i].daysWorked);
+									$scope.custServeArray[$scope.j][5] = parseInt($scope.staffPerfs[$scope.i].custServed);
+								} else if ($scope.staffPerfs[$scope.i].monthYear.slice(-2) === "07") {
+									$scope.daysworkArray[$scope.j][6] = parseInt($scope.staffPerfs[$scope.i].daysWorked);
+									$scope.custServeArray[$scope.j][6] = parseInt($scope.staffPerfs[$scope.i].custServed);
+								} else if ($scope.staffPerfs[$scope.i].monthYear.slice(-2) === "08") {
+									$scope.daysworkArray[$scope.j][7] = parseInt($scope.staffPerfs[$scope.i].daysWorked);
+									$scope.custServeArray[$scope.j][7] = parseInt($scope.staffPerfs[$scope.i].custServed);
+								} else if ($scope.staffPerfs[$scope.i].monthYear.slice(-2) === "09") {
+									$scope.daysworkArray[$scope.j][8] = parseInt($scope.staffPerfs[$scope.i].daysWorked);
+									$scope.custServeArray[$scope.j][8] = parseInt($scope.staffPerfs[$scope.i].custServed);
+								} else if ($scope.staffPerfs[$scope.i].monthYear.slice(-2).slice(-2) === "10") {
+									$scope.daysworkArray[$scope.j][9] = parseInt($scope.staffPerfs[$scope.i].daysWorked);
+									$scope.custServeArray[$scope.j][9] = parseInt($scope.staffPerfs[$scope.i].custServed);
+								} else if ($scope.staffPerfs[$scope.i].monthYear.slice(-2) === "11") {
+									$scope.daysworkArray[$scope.j][10] = parseInt($scope.staffPerfs[$scope.i].daysWorked);
+									$scope.custServeArray[$scope.j][10] = parseInt($scope.staffPerfs[$scope.i].custServed);
+								} else if ($scope.staffPerfs[$scope.i].monthYear.slice(-2) === "12") {
+									$scope.daysworkArray[$scope.j][11] = parseInt($scope.staffPerfs[$scope.i].daysWorked);
+									$scope.custServeArray[$scope.j][11] = parseInt($scope.staffPerfs[$scope.i].custServed);
+								}
+							}
 						}
+
 					}
 				}
+
 				for ($scope.f = 0; $scope.f < $scope.staffarray.length; $scope.f++) {
 					$scope.daysperformance.push({
 						name: $scope.staffarray[$scope.f].staffID,
@@ -205,57 +270,58 @@
 						stack: 'Customers Served'
 					});
 				}
+				if ($scope.yearSelect) {
+					Highcharts.chart('staffChart', {
 
-				Highcharts.chart('staffChart', {
+						chart: {
+							type: 'column'
+						},
 
-					chart: {
-						type: 'column'
-					},
-
-					title: {
-						text: 'Staff Performance'
-					},
-
-					xAxis: {
-						categories: ['Jan',
-							'Feb',
-							'Mar',
-							'Apr',
-							'May',
-							'Jun',
-							'Jul',
-							'Aug',
-							'Sep',
-							'Oct',
-							'Nov',
-							'Dec'
-						]
-					},
-
-					yAxis: {
-						allowDecimals: false,
-						min: 0,
 						title: {
-							text: 'Figures'
-						}
-					},
+							text: 'Staff Performance'
+						},
 
-					tooltip: {
-						formatter: function() {
-							return '<b>' + this.series.options.stack + '</b><br/>' +
-								this.series.name + ': ' + this.y + '<br/>' +
-								'Total: ' + this.point.stackTotal;
-						}
-					},
+						xAxis: {
+							categories: ['Jan',
+								'Feb',
+								'Mar',
+								'Apr',
+								'May',
+								'Jun',
+								'Jul',
+								'Aug',
+								'Sep',
+								'Oct',
+								'Nov',
+								'Dec'
+							]
+						},
 
-					plotOptions: {
-						column: {
-							stacking: 'normal'
-						}
-					},
+						yAxis: {
+							allowDecimals: false,
+							min: 0,
+							title: {
+								text: 'Figures'
+							}
+						},
 
-					series: $scope.daysperformance
-				});
+						tooltip: {
+							formatter: function() {
+								return '<b>' + this.series.options.stack + '</b><br/>' +
+									this.series.name + ': ' + this.y + '<br/>' +
+									'Total: ' + this.point.stackTotal;
+							}
+						},
+
+						plotOptions: {
+							column: {
+								stacking: 'normal'
+							}
+						},
+
+						series: $scope.daysperformance
+					});
+				}
 			}
 		});
 
