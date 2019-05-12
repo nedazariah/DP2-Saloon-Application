fbd388a276a2991d6a338b002ef799915835bff7
NixSinyem <36840973+NixSinyem@users.noreply.github.com>
2019-05-12 15:25:37 +0800

    Refactored code

--- service_module_delete.php
+++ service_module_delete.php
@@ -12,6 +12,14 @@ include "session_check.php";
 </head>
 <body> 
 <?php
+function errorProcess($message)
+{
+    mysqli_close($connect);
+    echo $message;
+    echo "</br></br>Redirecting back.";
+    header('Refresh: 3; url=service_module_display.php');
+} 
+    
 if(isset($_GET['target']) && !empty(trim($_GET['target'])))
 {
     $target = trim($_GET['target']);
@@ -20,21 +28,20 @@ if(isset($_GET['target']) && !empty(trim($_GET['target'])))
 
     if (mysqli_query($connect, $sql))
     {
+        mysqli_close($connect);
         header("location: service_module_display.php");
         exit();
     }
     else 
     {
-        echo "Error: " . $sql . "</br>" . die(mysqli_error($connect));
-        echo "Redirecting back.";
-        header('Refresh: 3; url=service_module_display.php');
+       
+        errorProcess("Error: " . $sql . "</br>" . die(mysqli_error($connect))); 
     }
 }
 else
 {
-    echo "Error: service ID not found.";
-    echo "Redirecting back.";
-    header('Refresh: 3; url=service_module_display.php');
+   
+    errorProcess("Error: service ID not found."); 
 }
 ?>
 </body>
